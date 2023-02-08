5feb8cee6e816124ae542d99d28ee3cc8ff65ef0
Aldi-880 <mtaldi880@gmail.com>
2023-02-08 22:51:13 +0700

    first commit

---
+++ application/views/depan/v_contact.php
@@ -0,0 +1,24 @@
+
+<section class="contact-section">
+            <div class="container">
+                <div class="d-none d-sm-block mb-5 pb-4">
+                    
+                                        
+                    
+                    <div class="row">
+                    <div class="col-md-12">
+                        
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15834.729836486931!2d112.70960312176547!3d-7.162660167252441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd8022f392617fd%3A0x6add6def150e2e7d!2sSD%20Negeri%20Banyuajuh%202!5e0!3m2!1sid!2sid!4v1675608029072!5m2!1sid!2sid" width="1000" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>                        
+                        <iframe style="width: 100vw;height: 100vh;position: relative;" src="" frameborder="0" allowfullscreen></iframe>
+                        
+                    
+                    </div>
+                    </div>
+                    
+    
+                </div>
+    
+    
+                
+            </div>
+        </section>
---
+++ application/views/theme/header.php
@@ -0,0 +1,19 @@
+
+            <div class="header-top_area">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="header_top_wrap d-flex justify-content-between align-items-center">
+                                <div class="text_wrap">
+                                    <p><span></span> | <span>Email : sdnbanyuajuh2kamal@gmail.com</span></p>
+                                </div>
+                                <div class="text_wrap">
+                                    <p><a href="<?php echo site_url('administrator');?>"> <i class="ti-user"></i>  Login</a> </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        
